57ed2cf94c73082601063130eebff6d938398c11
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-10-16 21:06:33 -0700

    modularize code

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
       <h1>Angular 2 ToDo List</h1>
-      <h3 (click)="showDetails(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
-      <div>
-        <h1>Edit Task</h1>
-        <div>
-          <label>Enter Task Description:</label>
-          <input [(ngModel)]="selectedTask.description">
+      <div class="row">
+        <div class="col-xs-6">
+        <task-list
+          [childTaskList]="masterTaskList"
+          (clickSender)="showDetails($event)"
+        ></task-list>
+        <edit-task
+          [childSelectedTask]="selectedTask"
+          (doneClickedSender)="finishedEditing()"
+        ></edit-task>
         </div>
-        <div>
-          <label>Enter Task Id:</label>
-          <input [(ngModel)]="selectedTask.id">
+        <div class="col-xs-6">
+          <pies></pies>
         </div>
       </div>
     </div>
@@ -22,19 +26,17 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public tasks: Task[] = [
-    new Task("Create To-Do List app.", 0),
-    new Task("Learn Kung Fu", 1),
-    new Task("Rewatch all the Lord of the Rings moies.", 2),
-    new Task("Do the laundry", 3)
+  public masterTaskList: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
   ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask: Task = null;
   showDetails(clickedTask: Task) {
     this.selectedTask = clickedTask;
   }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {}
+  finishedEditing() {
+    this.selectedTask = null;
+  }
 }
